2e2a420962a3c73f3019bba090a04cc96a406cc7
郑家骜[ào] <zhengja@dist.com.cn>
2025-03-06 17:32:01 +0800

    feat(vue3-package) 新增element模块

---
+++ vue3-package/vue3-package-element/src/views/UserView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// import UserManage from '../components/UserScriptComponent.vue'
+// import UserManage from '../components/UserScriptSetupComponent.vue'
+
+// import UserManage from '../components/UserAxiosComponent.vue'
+// import UserManage from '../components/UserAxiosElementComponent.vue'
+// import UserManage from '../components/UserAxiosElementEditingComponent.vue'
+import UserManage from '../components/UserAxiosElementPageComponent.vue'
+</script>
+
+<template>
+  <main>
+    <UserManage />
+  </main>
+</template>
